724a2db818f35a854c6a99cbd4e41bb38d412ff4
Алексей Чернышенко <tech.dep.it@ya.ru>
2020-09-17 17:56:10 +0300

    Update

--- tasks/10/provision.sh
+++ tasks/10/provision.sh
@@ -9,21 +9,25 @@ CLIENT1_IP='192.168.50.20'
 
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y tree wget
+
 # Установка часового пояса
 timedatectl set-timezone Europe/Moscow
+
+# Установка русской локали
+sudo echo "locales locales/default_environment_locale select ru_RU.UTF-8" | sudo debconf-set-selections
+sudo echo "locales locales/locales_to_be_generated multiselect ru_RU.UTF-8 UTF-8" | sudo debconf-set-selections
+sudo rm "/etc/locale.gen"
+sudo dpkg-reconfigure --frontend noninteractive locales
 
 case $HOSTNAME in
     $SRV)
         # Возможность использования имен серверов вместо IP-адресов
         echo "$CLIENT1_IP  $CLIENT1" >> /etc/hosts
 
-
-
         wget https://github.com/prometheus/prometheus/releases/download/v2.21.0/prometheus-2.21.0.linux-amd64.tar.gz &> /dev/null
         tar zxvf prometheus-*.linux-amd64.tar.gz && cd prometheus-*.linux-amd64
 
         sudo mkdir /etc/prometheus /var/lib/prometheus
-
 
         sudo cp prometheus promtool /usr/local/bin/
         sudo cp -r console_libraries consoles prometheus.yml /etc/prometheus
